6184be9c2dab642acff9de56895950ffcaa47828
tejasuresh <kaminitejasuresh@gmail.com>
2024-05-04 11:40:42 +0000

    fourth commit

---
+++ 04-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter username::"
+
+read -s USERNAME #here USERNAME is  variable
+
+echo "please enter password::"
+
+read -s PASSWORD
+
+echo "username is: $USERNAME, password is: PASSWORD"
