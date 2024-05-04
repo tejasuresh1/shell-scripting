2d7f221ad96281fbc2d270832a2e05aff8e593e9
tejasuresh <kaminitejasuresh@gmail.com>
2024-05-04 11:49:36 +0000

    array

---
+++ 05-array.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MOVIES=("RRR" "DEVARA" "SALAAR")
+
+#size of above array is 3.
+#index are 0,1,2
+
+
+#list always starts with 0.
+
+
+echo "first movie is: ${MOVIES[0]}"
+echo "secnod movie is: ${MOVIES[1]}"
+echo "thrid movie is: ${MOVIES[2]}"
