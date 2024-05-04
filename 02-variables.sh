0b368f0b41768409f72fb6ecd6be94d9622e73c0
tejasuresh <kaminitejasuresh@gmail.com>
2024-05-04 11:18:36 +0000

    second changes

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "suresh:: Hello teja, How are you?"
+echo "teja:: Hi suresh, I am fine. Hoe are you?"
+echo "Suresh:: I am fine too. how is your work?"
+echo "teja:: Not bad. I am upgrade my Devops skills"
