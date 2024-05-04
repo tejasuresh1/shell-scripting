f33466fbc3e281e4339595c1ac168238a32ce895
tejasuresh <kaminitejasuresh@gmail.com>
2024-05-04 11:34:38 +0000

    thrid commit

---
+++ 03-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#declaring of variable
+PERSON1=ravi  # no space between = and value
+PERSON2=ramesh
+
+
+#referring variable
+
+echo "$PERSON1:: Hello $PERSON2,How are you?"
+echo "$PERSON2:: Hi $PERSON1,I am fine. How are you doing?"
+echo "$PERSON1:: I am fine too. how is your work?"
+echo "$PERSON2:: not bad. Iam thinking to upgrade to devops"
+
